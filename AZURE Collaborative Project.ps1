ecf43ac00a74635ed634979d726a5b9a2b596b95
David Allen <davidallen55555@yahoo.com>
2021-12-14 17:37:13 -0700

    first commit

---
+++ AZURE Collaborative Project.ps1
@@ -0,0 +1,13 @@
+# AZURE Collaborative Project
+
+# 2 Domain Controllers; hosting AD w DNS
+# Several Hosts
+# DHCP
+# File Server
+
+# Sync to AZAD
+
+# Federation Services
+# for AD credentials for a 
+# SaaS platform app, integrated in Azure
+
