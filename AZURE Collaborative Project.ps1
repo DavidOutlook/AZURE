9f67c64eaff7960b258ab9cd6e0d3fb8759308b5
David Allen <davidallen55555@yahoo.com>
2021-12-14 17:40:00 -0700

    Overview

--- AZURE Collaborative Project.ps1
+++ AZURE Collaborative Project.ps1
@@ -11,3 +11,5 @@
 # for AD credentials for a 
 # SaaS platform app, integrated in Azure
 
+# Group Project - Create an enterprise environment in Azure, consisting of 2 Domain Controllers hosting AD, several hosts, DNS, DHCP, File Server, 
+# synch it to AZAD, use the federation services to utilize AD credentials for a SaaS platform app integrated in AZ.  
